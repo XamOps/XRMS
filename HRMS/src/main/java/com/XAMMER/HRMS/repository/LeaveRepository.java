fa38eb483f2cfd08558b0f338787945387947c68
Aditya <aditya@xammer.in>
2025-05-13 17:39:47 +0530

    Delete HRMS directory

--- HRMS/src/main/java/com/XAMMER/HRMS/repository/LeaveRepository.java
+++
@@ -1,9 +0,0 @@
-// package com.XAMMER.HRMS.repository;
-
-// //package com.xammer.hrms.repository;
-
-// import com.XAMMER.HRMS.model.Leave;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface LeaveRepository extends JpaRepository<Leave, Long> {
-// }
