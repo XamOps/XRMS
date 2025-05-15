d31e49bf4e156191f8ac8e64d5e34105201189a3
Akshay Arora <akshayarora2172@gamil.com>
2025-05-16 00:21:53 +0530

    NEW CODE

---
+++ src/main/java/com/XAMMER/HRMS/service/EmailService.java
@@ -0,0 +1,13 @@
+package com.XAMMER.HRMS.service;
+
+import java.time.LocalDate;
+
+public interface EmailService {
+    void sendLeaveRequestNotification(String to, String requesterName);
+    void sendLeaveRequestApprovalNotification(String to, String requesterName);
+    void sendLeaveRequestRejectionNotification(String to, String requesterName, String reason);
+        void sendLeaveRequestNotification(String to, String requesterName, LocalDate appliedDate, String reason);
+            void sendLeaveRequestNotification(String to, String requesterName, LocalDate startDate, LocalDate endDate, String reason);
+
+
+}
